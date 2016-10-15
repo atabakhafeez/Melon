58206c42561c467ab00f886ae1d5a8b99d8f6e8d
Atabak Hafeez <atabakhafeez@hotmail.com>
2016-10-15 20:06:46 +0200

    Add Facebook Api dependencies

--- app/src/main/java/com/aama/melon/MainActivity.java
+++ app/src/main/java/com/aama/melon/MainActivity.java
@@ -1,13 +1,19 @@
 package com.aama.melon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
     }
 }
