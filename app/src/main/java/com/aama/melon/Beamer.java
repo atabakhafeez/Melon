3419ebd5dc29fd2b5f15e35c50b202c3a43647c5
ThatMathGuy <m.tarigradschi@gmail.com>
2016-10-15 22:23:09 +0200

    Good fix

--- app/src/main/java/com/aama/melon/Beamer.java
+++ app/src/main/java/com/aama/melon/Beamer.java
@@ -20,24 +20,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class Beamer extends Activity
+public class Beamer
         implements OnNdefPushCompleteCallback {
-    NfcAdapter mNfcAdapter;
-    TextView textView;
     Activity parent;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // Check for available NFC Adapter
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if (mNfcAdapter == null) {
-            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-
-        mNfcAdapter.setOnNdefPushCompleteCallback(this,this);
-    }
 
     public NdefMessage createMessage(String text)
     {
@@ -53,33 +38,6 @@ public class Beamer extends Activity
     public void onNdefPushComplete(NfcEvent event)
     {
         Log.e("Sending ","Sent !!!");
-        mNfcAdapter.setNdefPushMessage(null,parent);
+        NfcAdapter.getDefaultAdapter(parent).setNdefPushMessage(null,parent,parent);
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            processIntent(getIntent());
-        }
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        // onResume gets called after this to handle the intent
-        processIntent(intent);
-    }
-
-    /**
-     * Parses the NDEF Message from the intent and prints to the TextView
-     */
-    void processIntent(Intent intent) {
-        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
-                NfcAdapter.EXTRA_NDEF_MESSAGES);
-        // only one message sent during the beam
-        NdefMessage msg = (NdefMessage) rawMsgs[0];
-        // record 0 contains the MIME type, record 1 is the AAR, if present
-        textView.setText(new String(msg.getRecords()[0].getPayload()));
-    }
-
 }
--- app/src/main/java/com/aama/melon/MainActivity.java
+++ app/src/main/java/com/aama/melon/MainActivity.java
@@ -3,6 +3,7 @@ package com.aama.melon;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     Beamer beamer;
     NfcAdapter nfcAdapter;
+    TextView textView;
 
     public void SendNfc(View v)
     {
@@ -22,23 +24,32 @@ public class MainActivity extends AppCompatActivity {
         nfcAdapter.setNdefPushMessage(msg,this);
     }
 
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        textView.setText(new String(msg.getRecords()[0].getPayload()));
+    }
+
     @Override
     public void onResume() {
         super.onResume();
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            beamer.processIntent(getIntent());
+            processIntent(getIntent());
         }
     }
 
     @Override
     public void onNewIntent(Intent intent) {
         // onResume gets called after this to handle the intent
-        beamer.processIntent(intent);
+        processIntent(intent);
     }
 
     public void cancel()
     {
-        nfcAdapter.setNdefPushMessage(null,this);
+        nfcAdapter.setNdefPushMessage(null,this,this);
     }
 
 
@@ -46,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.Text1);
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         beamer = new Beamer();
