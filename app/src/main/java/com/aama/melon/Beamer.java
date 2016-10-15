a7edb329d317903b9b23751a836883cc2b198fbf
ThatMathGuy <m.tarigradschi@gmail.com>
2016-10-15 20:44:18 +0200

    NFC first try

---
+++ app/src/main/java/com/aama/melon/Beamer.java
@@ -0,0 +1,92 @@
+package com.aama.melon;
+
+import android.app.Activity;
+import android.nfc.NfcAdapter;
+
+
+/**
+ * Created by mike on 10/15/16.
+ */
+
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter.OnNdefPushCompleteCallback;
+import android.nfc.NfcEvent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class Beamer extends Activity
+        implements OnNdefPushCompleteCallback {
+    NfcAdapter mNfcAdapter;
+    TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // Check for available NFC Adapter
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        // Register callback
+        mNfcAdapter.setOnNdefPushCompleteCallback(this, this);
+    }
+
+    public void SendMessage(String text) {
+        NdefMessage msg = createMessage(text);
+        mNfcAdapter.setNdefPushMessage(msg, this);
+    }
+
+    public NdefMessage createMessage(String text)
+    {
+        //string text = ("Beam me up, Android!\n\n" +
+        //        "Beam Time: " + System.currentTimeMillis());
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] { NdefRecord.createMime(
+                        "application/com.aama.melon", text.getBytes())
+                        , NdefRecord.createApplicationRecord("com.aama.melon")
+                });
+        return msg;
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event)
+    {
+        Log.e("Sending ","Sent !!!");
+        mNfcAdapter.setNdefPushMessage(null,this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        processIntent(intent);
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        textView.setText(new String(msg.getRecords()[0].getPayload()));
+    }
+
+}
+
--- app/src/main/java/com/aama/melon/MainActivity.java
+++ app/src/main/java/com/aama/melon/MainActivity.java
@@ -1,13 +1,48 @@
 package com.aama.melon;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Beamer beamer;
+    NfcAdapter nfcAdapter;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            beamer.processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        beamer.processIntent(intent);
+    }
+
+    public void SendNfc(View v)
+    {
+        String text = ((EditText) findViewById(R.id.editText)) . getText() .toString();
+        ((TextView) findViewById(R.id.Text1)) . setText("AAA");
+        NdefMessage msg = beamer.createMessage(text);
+        nfcAdapter.setNdefPushMessage(msg, this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        beamer = new Beamer();
+        beamer.textView = (TextView) findViewById(R.id.Text1);
     }
 }
