7dfd3a6e69373f611d781beb0c95ba965f81a9f1
Atabak Hafeez <atabakhafeez@hotmail.com>
2016-10-16 01:16:40 +0200

    Working Twitter log in

--- app/src/main/java/com/aama/melon/MainActivity.java
+++ app/src/main/java/com/aama/melon/MainActivity.java
@@ -1,12 +1,10 @@
 package com.aama.melon;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
@@ -14,6 +12,8 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
@@ -27,26 +27,56 @@ public class MainActivity extends FragmentActivity {
     private boolean twitter_logged_in;
     public static final String LOGIN_PREFS = "LoginPrefs";
     public static final String TWITTER = "TwitterLoggedIn";
+    public static final String TWITTER_KEY = "YvuU7TFgDRfLXgqixW3RDTYht";
+    public static final String TWITTER_SECRET = "ExODI9VwYzSQ4scaIztjZRqGXM9goyOpISW48L7T0VxIlChdLr";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Crashlytics());
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
-        twitterLogin();
-        SharedPreferences sharedPrefLogin = getSharedPreferences(LOGIN_PREFS,
-                Context.MODE_PRIVATE);
-        boolean twitter_logged_in = sharedPrefLogin.getBoolean(TWITTER, false);
+
         loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
 
-        if (twitter_logged_in) {
-            twitterLogin();
-        } else {
-            loginButton.setVisibility(View.GONE);
-        }
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+
+                SharedPreferences settings = getSharedPreferences(LOGIN_PREFS, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean(TWITTER, true);
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+
+//        twitterLogin();
+//        SharedPreferences sharedPrefLogin = getSharedPreferences(LOGIN_PREFS,
+//                Context.MODE_PRIVATE);
+//        twitter_logged_in = false;
+//        boolean twitter_logged_in = sharedPrefLogin.getBoolean(TWITTER, false);
+//        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+//
+//        if (twitter_logged_in) {
+//            twitterLogin();
+//        } else {
+//            loginButton.setVisibility(View.GONE);
+//            Switch twitter_switch = (Switch) findViewById(R.id.twitter_switch);
+//            twitter_switch.setVisibility(View.VISIBLE);
+//        }
 
     }
 
@@ -71,10 +101,6 @@ public class MainActivity extends FragmentActivity {
                 Log.d("TwitterKit", "Login with Twitter failure", exception);
             }
         });
-    }
-
-    updateUI() {
-
     }
 
     @Override
