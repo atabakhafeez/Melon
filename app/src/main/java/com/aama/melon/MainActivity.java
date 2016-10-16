27fb8f29c32b9b0fe543fa516bd273484b0e7b0d
ThatMathGuy <m.tarigradschi@gmail.com>
2016-10-16 11:04:37 +0200

    BAAAD twiitter

--- app/src/main/java/com/aama/melon/MainActivity.java
+++ app/src/main/java/com/aama/melon/MainActivity.java
@@ -19,7 +19,17 @@ import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import io.fabric.sdk.android.Fabric;
+
+import static android.R.attr.data;
 
 
 public class MainActivity extends FragmentActivity {
@@ -61,6 +71,72 @@ public class MainActivity extends FragmentActivity {
 
     }
 
+    private void twitteraddfollow(String id) throws IOException {
+
+        String s = "";
+        s += "user_id=";
+        s += id;
+        s += "&follow=true";
+
+        byte[] postData       = s.getBytes( StandardCharsets.UTF_8 );
+        int    postDataLength = postData.length;
+
+        String token = twitterSession.getAuthToken().toString();
+
+        String url = "https://api.twitter.com/1.1/friendships/create.json";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        con.setDoOutput( true );
+        con.setInstanceFollowRedirects( false );
+        con.setRequestMethod( "POST" );
+        con.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty( "charset", "utf-8");
+        con.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+        con.setUseCaches( false );
+        try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+            wr.write( postData );}
+        catch( Exception e)
+        {
+            Log.e("Error: ",e.toString());
+        }
+
+
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://<ip address>:3000");
+
+        try {
+            //add data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("data", data[0]));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            //execute http post
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    private void runtwitter(final String id)
+    {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    twitteraddfollow(id);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
     private void twitterLogin() {
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
@@ -77,6 +153,7 @@ public class MainActivity extends FragmentActivity {
 //                SharedPreferences settings = getSharedPreferences(LOGIN_PREFS, 0);
 //                SharedPreferences.Editor editor = settings.edit();
 //                editor.putBoolean(TWITTER, true);
+                runtwitter("82050329");
             }
             @Override
             public void failure(TwitterException exception) {
