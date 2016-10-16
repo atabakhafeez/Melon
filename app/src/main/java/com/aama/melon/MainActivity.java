4e368a5c04daa96435dc9b163efbe20acccd358b
Akbar Oripov <akbarzoripov@gmail.com>
2016-10-16 07:30:11 +0200

    Get data for FB user

--- app/src/main/java/com/aama/melon/MainActivity.java
+++ app/src/main/java/com/aama/melon/MainActivity.java
@@ -7,13 +7,20 @@ import android.os.Parcelable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
+import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.FacebookException;
+import com.facebook.CallbackManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.LoginManager;
+import com.facebook.FacebookCallback;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
@@ -38,6 +45,10 @@ public class MainActivity extends FragmentActivity {
     public static final String TWITTER_KEY = "YvuU7TFgDRfLXgqixW3RDTYht";
     public static final String TWITTER_SECRET = "ExODI9VwYzSQ4scaIztjZRqGXM9goyOpISW48L7T0VxIlChdLr";
 
+    public static CallbackManager callbackManager = null;
+    public static AccessToken accessToken = null;
+    public static final String FB_LOG_TAG = "Facebook Login";
+
     private String username;
     private Long id;
     TwitterSession twitterSession = null;
@@ -45,7 +56,7 @@ public class MainActivity extends FragmentActivity {
     public void SendNfc(View v)
     {
         String text = ((EditText) findViewById(R.id.editText)) . getText() .toString();
-        ((TextView) findViewById(R.id.Text1)) . setText("AAA");
+        ((TextView) findViewById(R.id.Text1)) . setText("Hello World!"); // TODO
         NdefMessage msg = beamer.createMessage(text);
         nfcAdapter.setNdefPushMessage(msg,this);
     }
@@ -85,6 +96,27 @@ public class MainActivity extends FragmentActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        accessToken = loginResult.getAccessToken();
+                        Log.d(FB_LOG_TAG, "Successful sigin");
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d(FB_LOG_TAG, "Logout");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.e(FB_LOG_TAG, "Sign-in error");
+                    }
+                });
 
         // twitter auth
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
